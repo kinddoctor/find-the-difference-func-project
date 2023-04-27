f7382f1b49dba26b6d53be8e4bc3cc0e158c155b
kinddoctor <tata-yagodina@yandex.ru>
2023-04-28 01:15:05 +0500

    create gendiff need to import commander

---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander.js';
+
+const program = new Command();
+
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference')
+  .version('1.0.0', '-v, --version', 'output the version number');
+
+program.parse();
