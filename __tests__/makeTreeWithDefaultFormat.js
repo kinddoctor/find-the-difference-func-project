c04ace2bcc425fc229ea89c6b24a2414d137aa7d
kinddoctor <tata-yagodina@yandex.ru>
2023-06-14 10:52:24 +0500

    add test for nested files

--- __tests__/makeTreeWithDefaultFormat.js
+++ __tests__/makeTreeWithDefaultFormat.js
@@ -2,22 +2,22 @@ import { readFileSync } from 'node:fs';
 import path from 'node:path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import gendiff from '../src/gendiff.-OldPlainVersionjs';
+import gendiff from '../bin/gendiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 let expectedResult;
-let file1;
-let file2;
+let filepath1;
+let filepath2;
 
 beforeEach(() => {
-  expectedResult = readFileSync(getFixturePath('expected_file.txt'), 'utf-8');
-  file1 = getFixturePath('file1.json');
-  file2 = getFixturePath('file2.json');
+  expectedResult = readFileSync(getFixturePath('expectedNestedDiff.txt'), 'utf-8');
+  filepath1 = getFixturePath('file1.yml');
+  filepath2 = getFixturePath('file2.yml');
 });
 
 test('genDiff', () => {
-  expect(gendiff(file1, file2)).toEqual(expectedResult);
+  expect(gendiff(filepath1, filepath2)).toEqual(expectedResult);
 });
