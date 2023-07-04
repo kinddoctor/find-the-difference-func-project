0472b9a24c5dbc55dbc222e61ad72147c32c6063
kinddoctor <tata-yagodina@yandex.ru>
2023-07-04 16:14:07 +0500

    update stylish

--- src/stylish-formater.js
+++ src/stylish-formater.js
@@ -1,29 +1,49 @@
 const makeStylish = (diffTree, spaceCount = 4) => {
   const iter = (tree, depth) => {
-    const keys = Object.keys(tree);
+    const keys = Object.keys(tree).sort();
     const indent = ' '.repeat(depth * spaceCount - 2);
     const bracketIndent = ' '.repeat(depth * spaceCount - spaceCount);
     const specialSigns = { added: '+ ', deleted: '- ', unchanged: '  ' };
     const lines = keys.map((key) => {
-      switch (key.status) {
-        case 'added': {
-          return `${indent}${specialSigns.added}${key}: ${key.value}`;
+      let value;
+      let value1;
+      let value2;
+      switch (true) {
+        case (typeof tree[key].value !== 'object' || tree[key].value === null): {
+          value = tree[key].value;
+          break;
         }
-        case 'deleted': {
-          return `${indent}${specialSigns.deleted}${key}: ${key.value}`;
+        case tree[key].value.first: {
+          value1 = (typeof tree[key].value.first !== 'object' || tree[key].value.first === null) ? tree[key].value.first : iter(tree[key].value.first, depth + 1);
+          value2 = (typeof tree[key].value.second !== 'object' || tree[key].value.second === null) ? tree[key].value.second : iter(tree[key].value.second, depth + 1);
+          break;
         }
-        case 'changed': {
-          return `${indent}${specialSigns.deleted}${key}: ${key.value.first}\n
-          ${indent}${specialSigns.added}${key}: ${key.value.second}`;
-        }
-        case 'unchanged': {
-          return `${indent}${specialSigns.unchanged}${key}: ${key.value}`;
-        }
-        case 'nested': {
-          return `${indent}${specialSigns.unchanged}${key}: ${iter(key.value, depth + 1)}`;
+        case (typeof tree[key].value === 'object'): {
+          value = iter(tree[key].value, depth + 1);
+          break;
         }
         default: {
-          throw new Error(`Unknown status: ${key.status} in key ${key}`);
+          value = tree[key].value;
+        }
+      }
+      switch (tree[key].status) {
+        case 'added': {
+          return `${indent}${specialSigns.added}${key}: ${value}`;
+        }
+        case 'deleted': {
+          return `${indent}${specialSigns.deleted}${key}: ${value}`;
+        }
+        case 'changed': {
+          return `${indent}${specialSigns.deleted}${key}: ${value1}\n${indent}${specialSigns.added}${key}: ${value2}`;
+        }
+        case 'unchanged': {
+          return `${indent}${specialSigns.unchanged}${key}: ${value}`;
+        }
+        case 'nested': {
+          return `${indent}${specialSigns.unchanged}${key}: ${value}`;
+        }
+        default: {
+          return `${indent}${specialSigns.unchanged}${key}: ${value}`;
         }
       }
     });
