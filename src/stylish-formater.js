6cbec2902cd14a522fbfe2ea8e9336e93d8bf349
kinddoctor <tata-yagodina@yandex.ru>
2023-07-05 19:49:06 +0500

    make finally working stylish formater!

--- src/stylish-formater.js
+++ src/stylish-formater.js
@@ -5,52 +5,34 @@ const makeStylish = (diffTree, spaceCount = 4) => {
     const bracketIndent = ' '.repeat(depth * spaceCount - spaceCount);
     const specialSigns = { added: '+ ', deleted: '- ', unchanged: '  ' };
     const lines = keys.map((key) => {
-      let value;
-      let value1;
-      let value2;
-      switch (true) {
-        case (typeof tree[key].value !== 'object' || tree[key].value === null): {
-          value = tree[key].value;
-          break;
-        }
-        case tree[key].value.first: {
-          value1 = (typeof tree[key].value.first !== 'object' || tree[key].value.first === null) ? tree[key].value.first : iter(tree[key].value.first, depth + 1);
-          value2 = (typeof tree[key].value.second !== 'object' || tree[key].value.second === null) ? tree[key].value.second : iter(tree[key].value.second, depth + 1);
-          break;
-        }
-        case (typeof tree[key].value === 'object'): {
-          value = iter(tree[key].value, depth + 1);
-          break;
-        }
-        default: {
-          value = tree[key].value;
-        }
-      }
+      const getValue = (value) => {
+        const finalValue = (typeof value !== 'object' || value === null) ? value : iter(value, depth + 1);
+        return finalValue;
+      };
       switch (tree[key].status) {
         case 'added': {
-          return `${indent}${specialSigns.added}${key}: ${value}`;
+          return `${indent}${specialSigns.added}${key}: ${getValue(tree[key].value)}`;
         }
         case 'deleted': {
-          return `${indent}${specialSigns.deleted}${key}: ${value}`;
+          return `${indent}${specialSigns.deleted}${key}: ${getValue(tree[key].value)}`;
         }
         case 'changed': {
-          return `${indent}${specialSigns.deleted}${key}: ${value1}\n${indent}${specialSigns.added}${key}: ${value2}`;
+          return `${indent}${specialSigns.deleted}${key}: ${getValue(tree[key].oldValue)}\n${indent}${specialSigns.added}${key}: ${getValue(tree[key].newValue)}`;
         }
         case 'unchanged': {
-          return `${indent}${specialSigns.unchanged}${key}: ${value}`;
+          return `${indent}${specialSigns.unchanged}${key}: ${getValue(tree[key].value)}`;
         }
         case 'nested': {
-          return `${indent}${specialSigns.unchanged}${key}: ${value}`;
+          return `${indent}${specialSigns.unchanged}${key}: ${getValue(tree[key].value)}`;
         }
         default: {
-          return `${indent}${specialSigns.unchanged}${key}: ${value}`;
+          return `${indent}${specialSigns.unchanged}${key}: ${getValue(tree[key])}`;
         }
       }
     });
     return ['{',
       ...lines,
-      bracketIndent,
-      '}'].join('\n');
+      `${bracketIndent}}`].join('\n');
   };
   return iter(diffTree, 1);
 };
